849f37e69669709b4baa68be27a89e0aebe38758
Oscar marina <oscar7600@outlook.com>
2016-12-21 09:56:43 +0100

    feat(html): remove old ref. in base.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 /* global Rx */
 //import Snap from 'imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js';
-Rx.Observable
+/*Rx.Observable
   .interval(200)
   .take(9)
   .map(x => x + '!!!')
   .bufferCount(3)
-  .subscribe(console.log);
+  .subscribe(console.log);*/
